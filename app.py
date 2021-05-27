871ceabdf0f8508c723b52dbce9377911b3cc560
debskiszymon <debski.szymon@gmail.com>
2021-05-27 12:55:32 +0100

    slider update

--- app.py
+++ app.py
@@ -19,13 +19,16 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 df_genre = pd.read_csv('df_movies_genre.csv')
 df_dash = pd.read_csv('df_movies_dash.csv')
 
-pull = []
-for _ in df_genre.groupby('genre'):
-    pull.append(0.1)
+def graph_pull(df):
+    pull = []
+    for _ in df.groupby('genre'):
+        pull.append(0.1)
 
-for index, element in enumerate(pull):
-    if index % 2 == 0:
-        pull[index] = element * 3
+    for index, element in enumerate(pull):
+        if index % 2 == 0:
+            pull[index] = element * 3
+
+    return pull
 
 colors = ['rgb(190, 219, 57)',
 'rgb(157, 213, 67)',
@@ -48,7 +51,16 @@ colors = ['rgb(190, 219, 57)',
 'rgb(0, 45, 97)',
 'rgb(0, 34, 83)']
 
-mark_values = {1874: '1874', 1900: '1900', 2000: '2000', 2021: '2021'}
+def year_list(df):
+    year = list(df['release_year'].unique())
+    return year
+# year_list = list(year)
+# year_keys = [int(i) for i in list(year)[::3]]
+# year_values = [str(i) for i in list(year)[::3]]
+# zip_iterator = zip(year_keys, year_values)
+# year_dict = dict(zip_iterator)
+
+# mark_values = year_dict
 
 # grouped = df_genre.groupby('genre').vote_average.mean()
 # fig1 = go.Figure(data=[go.Pie(labels=grouped.index, 
@@ -69,18 +81,20 @@ mark_values = {1874: '1874', 1900: '1900', 2000: '2000', 2021: '2021'}
 #     height=800,
 # title='Average votes by Genre')
 
-fig2 = go.Figure([go.Bar(x=df_dash.nlargest(10, 'vote_count').original_title, 
-                        y=df_dash.nlargest(10, 'vote_count').vote_count)])
+# fig2 = go.Figure([go.Bar(x=df_dash.nlargest(10, 'vote_count').original_title, 
+#                         y=df_dash.nlargest(10, 'vote_count').vote_count)])
 
-fig2.update_traces(marker_color=colors, 
-                  marker_line_color='rgb(0,0,0)',
-                  marker_line_width=1)
+# fig2.update_traces(marker_color=colors, 
+#                   marker_line_color='rgb(0,0,0)',
+#                   marker_line_width=1)
 
-fig2.update_layout(
-    autosize=False,
-    width=1000,
-    height=600,
-title='Highest vote count')
+# fig2.update_layout(
+#     autosize=False,
+#     width=1000,
+#     height=600,
+# title='Highest vote count')
+
+
 
 app.layout = html.Div(children=[
     html.H1(children='TMDB Movies',style={'text-align':'center'}),
@@ -99,16 +113,20 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.RangeSlider(
             id='the_year',
+            size = 50,
             # marks={i: '{}'.format(10 ** i) for i in range(4)},
-            min = 1874, 
-            max= 2021,
-            value=[1874, 2021],
-            marks= mark_values,
+            min = year_list(df_genre)[-1], 
+            max= year_list(df_genre)[0],
+            value=[year_list(df_genre)[-1], year_list(df_genre)[0]],
+            # marks= mark_values,
+            # marks={i : {'label' : str(year_list[i]), 'style':{'transform':'rotate(-90deg)'}} for i in range(0, len(year_list)-1)},
+            # marks={each : {'label': year, 'style': {'transform': 'rotate(45deg)'}} for each, year in enumerate(year_dict)},
+            marks={int(i) : {"label": str(i), "style": {"transform": "rotate(45deg)", 'font-size':'11px'}} for i in year_list(df_genre)[::2]},
             step=None,
             # updatemode='drag'
         ),
-    # html.Div(id='updatemode-output-container', style={'margin-top': 20})
-])
+    # html.Div(id='updatemode-output-container', style={'margin-top': 20, 'width':'50%'})
+], style={'width': '90%','padding-left':'5%', 'padding-right':'5%', 'opacity': '0.8'})
 ])
 
 
@@ -126,7 +144,7 @@ def update_fig1(year_chosen):
     fig1 = go.Figure(data=[go.Pie(labels=grouped.index, 
                               values=grouped.values, 
                               texttemplate = "%{label} %{value:.1f}", 
-                              pull=pull)])
+                              pull=graph_pull(df_genre))])
 
 
     fig1.update_traces(marker=dict(colors=colors, 
@@ -143,7 +161,31 @@ def update_fig1(year_chosen):
 
     return fig1
 
+    # ------------------------
 
+@app.callback(
+    Output('fig2', 'figure'),
+    [Input('the_year', 'value')]
+)
+
+def update_fig2(year_chosen):
+    # print(year_chosen)
+    dff_dash = df_dash[(df_dash['release_year']>=year_chosen[0])&(df_dash['release_year']<=year_chosen[1])]
+
+    fig2 = go.Figure([go.Bar(x=dff_dash.nlargest(10, 'vote_count').original_title, 
+                            y=dff_dash.nlargest(10, 'vote_count').vote_count)])
+
+    fig2.update_traces(marker_color=colors, 
+                    marker_line_color='rgb(0,0,0)',
+                    marker_line_width=1)
+
+    fig2.update_layout(
+        autosize=False,
+        width=1000,
+        height=600,
+    title='Highest vote count')
+
+    return fig2
 
 if __name__ == '__main__':
     app.run_server()
